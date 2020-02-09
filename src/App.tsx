ac902829063db9d6592d21095713bafedf575f38
Natalie Mclaren <>
2020-02-09 22:45:00 +0000

    Added axios call to github api and assigned latest release version to state

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useState } from 'react';
+import axios from 'axios';
+//import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps {
+}
+
+interface IState {
+  release?: string;
+  data?: object[];
+}
+
+export class App extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {};
+}
+
+  componentDidMount = () => {
+    let url = `https://api.github.com/repos/facebook/react/tags`;
+    let lastRelease;
+
+    axios.get(url, {headers: {'Authorization': "6df74f8774e1b665b3fbef4e0d116386cf915d73"}})
+      .then((response: any) => {
+          console.log(response.data);
+          lastRelease = response.data[0].name.substr(1);
+
+          if(this.state.release != lastRelease) {
+            this.setState({
+              release: lastRelease
+            });
+          } 
+      })
+      .catch((error: any) => {
+          console.log(error);
+      });
+
+      
+      
+  }
+  
+  render () {
+    console.log(this.state.release);
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
