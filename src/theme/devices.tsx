c24ccfcf93edeb48969df23fabf748476d1b58a7
Natalie Mclaren <>
2020-10-14 21:49:41 +0200

    added api urls

---
+++ src/theme/devices.tsx
@@ -0,0 +1,14 @@
+import sizes from './sizes';
+
+export default {
+    mobileS: `(min-width: ${sizes.mobileS})`,
+    mobileSMax: `(max-width: ${sizes.mobileS})`,
+    mobileM: `(min-width: ${sizes.mobileM})`,
+    mobileMMax: `(max-width: ${sizes.mobileM})`,
+    mobileL: `(min-width: ${sizes.mobileL})`,
+    tablet: `(min-width: ${sizes.tablet})`,
+    laptop: `(min-width: ${sizes.laptop})`,
+    laptopL: `(min-width: ${sizes.laptopL})`,
+    desktop: `(min-width: ${sizes.desktop})`,
+    desktopL: `(min-width: ${sizes.desktop})`
+}
---
+++ src/theme/sizes.tsx
@@ -0,0 +1,10 @@
+export default {
+    mobileS: '320px',
+    mobileM: '375px',
+    mobileL: '425px',
+    tablet: '768px',
+    laptop: '1024px',
+    laptopL: '1439px',
+    desktop: '1920px',
+    desktopL: '2560px'
+}
