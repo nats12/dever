6d931c7bdd2a7d47c7929a02067a5675580226a9
Natalie Mclaren <>
2020-10-14 21:53:57 +0200

    removed auth0

--- src/index.tsx
+++ src/index.tsx
@@ -8,16 +8,9 @@ import history from "./utils/history";
 
 
 ReactDOM.render(
-    <Auth0Provider
-      domain="dev-2hb7anjl.eu.auth0.com"
-      client_id="lz0RNj4bJWafSi3fgCHMconX6TJQg6hj"
-      redirect_uri="http://localhost:3001"
-      audience="http://localhost:8000/"
-    >
-      <App />
-    </Auth0Provider>,
-    document.getElementById("root")
-  );
+  <App />,
+  document.getElementById("root")
+);
 
 
 // If you want your app to work offline and load faster, you can change
