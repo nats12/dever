00fcb5d6cf797a2680e8ecdaa022c9bf877fa877
Natalie Mclaren <>
2020-10-14 20:46:01 +0200

    footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,13 +8,27 @@ const FooterBlock = styled.div`
     width: 100%;
     height: 20vh;
     padding-top: 25px;
+    overflow: hidden;
+`;
+
+const FooterContent = styled.div`
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+    padding: 0 30px;
+}
 `;
 
 export const Footer: React.FC = () => {
 
     return (
         <FooterBlock>
-            &copy; Natalie Mclaren 2020
+            <FooterContent>
+                <small>&copy; Natalie Mclaren 2020</small>
+                <small>Made with &hearts; by <a href="https://github.com/nats12" target="_blank">nats12</a></small>
+            </FooterContent>
         </FooterBlock>
     )
 }
